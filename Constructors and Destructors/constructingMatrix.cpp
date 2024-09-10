addaf83257c6c6765f607c3e2f8c94415ba8dd48
Rehnoor Aulakh <aulakhrehnoor@gmail.com>
2024-09-10 13:02:58 +0530

    matrix using constructors

---
+++ Constructors and Destructors/constructingMatrix.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+class Matrix
+{
+    int **p;
+    int d1,d2;
+    public:
+    Matrix(int x, int y);
+    void get_element(int i, int j, int value)
+    {
+        p[i][j]=value;
+    }
+    int& put_element(int i, int j)
+    {
+        return p[i][j];
+    }
+};
+
+Matrix::Matrix(int x, int y)
+{
+    d1=x;
+    d2=y;
+    p=new int *[d1];        //creates an array of integer pointers
+    for(int i=0;i<d1;i++)
+    {
+        p[i]=new int[d2];   //allocates memory for each row
+    }
+}
+
+int main()
+{
+    int m,n;
+
+    cout<<"Enter the size of matrix: ";
+    cin>>m>>n;
+    Matrix A(m,n);
+    cout<<"Enter Matrix Elements row by row\n";
+    int i,j,value;
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            cin>>value;
+            A.get_element(i,j,value);
+        }
+        cout<<endl;
+    }
+    cout<<A.put_element(1,2);
+}
